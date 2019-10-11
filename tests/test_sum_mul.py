8f90671f806c21b58b530379d084a4dd79c5dfb1
Opal Koren <44982787+OpalKo93@users.noreply.github.com>
2019-10-12 00:18:13 +0300

    added more tests

--- tests/test_sum_mul.py
+++ tests/test_sum_mul.py
@@ -1,4 +1,3 @@
-import app
 from arithmetic import sum_strings, mul_strings
 from unittest import TestCase
 
@@ -8,6 +7,24 @@ class TestSumMul(TestCase):
         num1 = "32"
         num2 = "13"
         expected_result = [4, 5]
+        self.assertEqual(sum_strings(num1, num2), expected_result)
+
+    def test_sum_with_none1(self):
+        num1 = ""
+        num2 = "537"
+        expected_result = [5, 3, 7]
+        self.assertEqual(sum_strings(num1, num2), expected_result)
+
+    def test_sum_with_none2(self):
+        num1 = "321"
+        num2 = ""
+        expected_result = [3, 2, 1]
+        self.assertEqual(sum_strings(num1, num2), expected_result)
+
+    def test_sum_with_both_none(self):
+        num1 = ""
+        num2 = ""
+        expected_result = []
         self.assertEqual(sum_strings(num1, num2), expected_result)
 
     def test_sum_with_carry_out(self):
@@ -23,9 +40,9 @@ class TestSumMul(TestCase):
         self.assertEqual(sum_strings(num1, num2), expected_result)
 
     def test_sum_with_big_nums(self):
-        num1 = "81241251325"
-        num2 = "99999999999"
-        expected_result = [1, 8, 1,2, 4, 1,2, 5, 1, 3, 2, 4]
+        num1 = "8124125132532423432431433426362143342174"
+        num2 = "999999243279636214334217481"
+        expected_result = [8, 1, 2, 4, 1, 2, 5, 1, 3, 2, 5, 3, 3, 4, 2, 3, 4, 3, 1, 6, 7, 4, 7, 1, 3, 0, 6, 2, 5, 7, 6, 4, 7, 7, 5, 5, 9, 6, 5, 5]
         self.assertEqual(sum_strings(num1, num2), expected_result)
 
     def test_mul_result(self):
@@ -47,11 +64,26 @@ class TestSumMul(TestCase):
         self.assertEqual(mul_strings(num1, num2), expected_result)
 
     def test_mul_big_nums(self):
-        num1 = "999"
-        num2 = "999"
-        expected_result = [9, 9, 8, 0, 0, 1]
+        num1 = "9934543747312321466784252345265479"
+        num2 = "1241312898745643999"
+        expected_result = [1, 2, 3, 3, 1, 8, 7, 7, 2, 9, 6, 6, 9, 1, 6, 7, 0, 3, 9, 9, 0, 2, 7, 9, 7, 5, 8, 9, 9, 9, 3, 1, 3, 4, 3, 9, 0, 5, 7, 9, 7, 0, 2, 2, 4, 7, 8, 2, 1, 0, 5, 2, 1]
         self.assertEqual(mul_strings(num1, num2), expected_result)
 
+    def test_mul_with_none1(self):
+        num1 = ""
+        num2 = "321"
+        expected_result = [3, 2, 1]
+        self.assertEqual(mul_strings(num1, num2), expected_result)
 
+    def test_mul_with_none2(self):
+        num1 = "321"
+        num2 = ""
+        expected_result = [3, 2, 1]
+        self.assertEqual(mul_strings(num1, num2), expected_result)
 
+    def test_mul_with_both_none(self):
+        num1 = ""
+        num2 = ""
+        expected_result = []
+        self.assertEqual(mul_strings(num1, num2), expected_result)
 
